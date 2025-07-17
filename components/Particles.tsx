6ac9c25adcc3f2f7e959e0cb0d678367fab0a61c
kayf <kayfgithub@gmail.com>
2025-07-17 01:02:04 -0300

    bug fixes

--- components/Particles.tsx
+++ components/Particles.tsx
@@ -228,6 +228,7 @@ const Particles: React.FC<ParticlesProps> = ({
     particleCount,
     particleSpread,
     speed,
+    particleColors,
     moveParticlesOnHover,
     particleHoverFactor,
     alphaParticles,
